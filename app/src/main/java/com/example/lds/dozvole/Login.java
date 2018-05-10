7ad8280f5a72c06509292db789aa287854110e86
LuisMontero1993 <lamontero@unac.edu.co>
2018-05-10 16:59:24 -0500

    reparando login

--- app/src/main/java/com/example/lds/dozvole/Login.java
+++ app/src/main/java/com/example/lds/dozvole/Login.java
@@ -1,10 +1,13 @@
 package com.example.lds.dozvole;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -14,6 +17,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class Login extends AppCompatActivity {
@@ -64,6 +68,7 @@ public class Login extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         //checking if success
                         if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
                             Toast.makeText(Login.this, "Bienvenido a Dozvole", Toast.LENGTH_LONG).show();
                             Intent intencion = new Intent(getApplication(), contenidos.class);
                             startActivity(intencion);
@@ -76,6 +81,7 @@ public class Login extends AppCompatActivity {
                             }
                         }
                         cargar.setVisibility(View.INVISIBLE);
+
                     }
                 });
 
